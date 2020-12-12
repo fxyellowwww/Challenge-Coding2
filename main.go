6284a5ca422aa810ec9360629649b464fb3d09b6
fxyellowwww <superfang.190944@gmail.com>
2020-12-13 02:28:26 +0700

    two hours

--- main.go
+++ main.go
@@ -11,3 +11,30 @@ func main() {
 		}
 	}
 }
+
+func a() {
+	var f float64
+	fmt.Scanf("%E", &f)
+	fmt.Printf("%E", f)
+}
+
+func b() {
+	var d int
+	fmt.Scanf("%d", &d)
+	fmt.Printf("%b", d)
+}
+
+func c() {
+	var d int
+	var f float64
+	fmt.Scanf("%d %E", &d, &f)
+	fmt.Printf("%b %E", d, f)
+}
+
+func d() {
+	var d int
+	var f float64
+	var s string
+	fmt.Scanf("%d %E %s", &d, &f, &s)
+	fmt.Printf("%T %T %T", d, f, s)
+}
