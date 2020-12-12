82d6134db861919c260fd7968c44b5a860188e04
fxyellowwww <superfang.190944@gmail.com>
2020-12-13 02:19:37 +0700

    second commit

--- main.go
+++ main.go
@@ -3,12 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	elements := make(map[string]string)
-	elements["H"] = "Hydrogen"
-	elements["He"] = "Helium"
-	elements["Li"] = "Lithium"
-	fmt.Println(elements)
-
-	delete(elements, "H")
-	fmt.Println(elements)
+	for i := 1; i <= 1000; i++ {
+		if i%3 == 0 {
+			fmt.Println(i, "fizz")
+		} else if i%5 == 0 {
+			fmt.Println(i, "buzz")
+		}
+	}
 }
