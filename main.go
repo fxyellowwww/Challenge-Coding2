9ffbee5ed5f62b44d5ab66f0be8077c23bf8cdd4
fxyellowwww <superfang.190944@gmail.com>
2020-12-13 11:31:57 +0700

    2+1hours

--- main.go
+++ main.go
@@ -3,6 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	var number int
+START:
+	fmt.Println("input number : ")
+	fmt.Scan(&number)
+	if number >= 500 && number <= 9000 {
+		for i := 3; i <= number; i = i + 2 {
+			fmt.Println(i)
+		}
+	} else {
+		fmt.Println("invalid number")
+		goto START
+	}
+}
+
+func b() {
 	for i := 1; i <= 1000; i++ {
 		if i%3 == 0 {
 			fmt.Println(i, "fizz")
@@ -12,29 +27,36 @@ func main() {
 	}
 }
 
-func a() {
+func c() {
 	var f float64
 	fmt.Scanf("%E", &f)
 	fmt.Printf("%E", f)
 }
 
-func b() {
+func d() {
 	var d int
 	fmt.Scanf("%d", &d)
 	fmt.Printf("%b", d)
 }
 
-func c() {
+func e() {
 	var d int
 	var f float64
 	fmt.Scanf("%d %E", &d, &f)
 	fmt.Printf("%b %E", d, f)
 }
 
-func d() {
+func f() {
 	var d int
 	var f float64
 	var s string
 	fmt.Scanf("%d %E %s", &d, &f, &s)
 	fmt.Printf("%T %T %T", d, f, s)
+}
+
+func g() {
+	x := []int{248, 428, 824}
+	for _, v := range x {
+		fmt.Printf("%d\n", v)
+	}
 }
